c204c9a3757a9dc879ff4496c82bb35061c03787
David Romero <d.romero@sapalomera.cat>
2026-05-29 21:16:50 +0200

    fix(auth): update logout redirect to use new /auth/login path

--- src/app/account/helpers/account-helpers.js
+++ src/app/account/helpers/account-helpers.js
@@ -4,7 +4,7 @@ export async function handleLogout(router) {
   try {
     await axios.post("/api/auth/logout");
 
-    router.push("/login");
+    router.push("/auth/login");
     router.refresh();
   } catch (error) {
     console.log("Ha habido un error a la hora de desloguearte", error)
